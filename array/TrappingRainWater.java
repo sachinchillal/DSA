27809d95359248aa1a40736386c23b8e62f18c80
sachinchillal <sachinachillal98a@gmail.com>
2025-04-19 19:01:21 +0530

    Add interactive input and table display for Trapping Rain Water animation

---
+++ array/TrappingRainWater.java
@@ -0,0 +1,65 @@
+class TestCase {
+  int[] A;
+  int R;
+
+  TestCase(int[] A, int R) {
+    this.A = A;
+    this.R = R;
+  }
+}
+
+public class TrappingRainWater {
+  public static void main(String[] args) {
+    System.err.println();
+    TestCase[] TestCases = {
+        new TestCase(new int[] { 1 }, 0),
+        new TestCase(new int[] { 2 }, 0),
+        new TestCase(new int[] { 3 }, 0),
+        new TestCase(new int[] { -5 }, 0),
+        new TestCase(new int[] { 1, 2 }, 0),
+        new TestCase(new int[] { 2, 3 }, 0),
+        new TestCase(new int[] { 1, 3 }, 0),
+        new TestCase(new int[] { 2, 3, 1, 2 }, 1),
+        new TestCase(new int[] { 2, 3, 1, 5, 6 }, 2),
+        new TestCase(new int[] { 1, 5, 6, 7, 3, 2 }, 0),
+        new TestCase(new int[] { 1, 5, 6, 7, 3, 8, 9, 10, 2 }, 4),
+        new TestCase(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }, 6),
+        new TestCase(new int[] { 1, 1, 3, 2, 1, 2, 5, 3, 2, 1, 4, 2 }, 10),
+    };
+    int count = 1;
+    for (TestCase testCase : TestCases) {
+      int[] A = testCase.A;
+      int expected = testCase.R;
+      int result = calculateWater(A);
+      if (result == expected) {
+        System.out.println(count + " Test case Passed!");
+      } else {
+        System.out.println(count + " Test case failed!");
+        System.out.println("Expected: " + expected + ", Result: " + result + "\n");
+      }
+      count++;
+    }
+  }
+
+  private static int calculateWater(int[] A) {
+    int water = 0;
+    int n = A.length;
+    int left = 0;
+    int right = n - 1;
+    int leftMax = 0;
+    int rightMax = 0;
+    while (left < right) {
+      leftMax = Math.max(A[left], leftMax);
+      rightMax = Math.max(A[right], rightMax);
+      if (leftMax < rightMax) {
+        water += leftMax - A[left];
+        left++;
+      } else {
+        water += rightMax - A[right];
+        right--;
+      }
+    }
+    return water;
+  }
+
+}
